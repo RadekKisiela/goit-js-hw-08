bcfa8d05b3e89b0c82605fd39e6f3cc41a658716
RadekKisiela <radek1997_97@o2.pl>
2023-05-06 14:13:18 +0200

    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,5 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -7,27 +9,22 @@ const gallery = document.querySelector('.gallery');
 
 function createGalleryItem(items) {
   return items
-    .map(
-      item =>
-        `<li class="gallery__item">
-                <a class="gallery__link" href="${item.original}">
-                    <img
-                    class="gallery__image"
-                    src="${item.preview}"
-                    data-source="${item.original}"
-                    alt="${item.description}">
-                </a>
-            </li>`
-    )
-    .join('');
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__link">
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `
+  )
+  .join(' ');
 }
 
 const addGalleryItem = createGalleryItem(galleryItems);
 
 gallery.innerHTML = addGalleryItem;
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   /* options */
