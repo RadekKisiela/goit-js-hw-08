3d8403583ce410eef65abb1c9f4c687d6748df58
RadekKisiela <radek1997_97@o2.pl>
2023-05-06 14:27:33 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveTimeLocalStorage = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+const getTimeFromLocalStorage = () => {
+  return localStorage.getItem('videoplayer-current-time');
+};
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(time => {
+      saveTimeLocalStorage(time);
+    });
+  }, 1000)
+);
+
+const storedTime = getTimeFromLocalStorage();
+
+if (storedTime !== null) {
+  player.setCurrentTime(storedTime);
+}
